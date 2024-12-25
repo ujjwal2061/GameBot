9a13b5809ee64a93e3668121a85a7f638ec6273c
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2024-12-25 19:12:38 +0530

    Add the Toogle Provider

--- game/src/main.jsx
+++ game/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { FirebaseProvider } from "./Auth/Fireauth.jsx"
 import App from './App.jsx'
+import ToogleProvider from './Auth/Toogle.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <FirebaseProvider>
+    <ToogleProvider>
     <App />
+    </ToogleProvider>
    </FirebaseProvider>
  </StrictMode>
 )
