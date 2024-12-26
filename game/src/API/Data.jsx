21ac06143ee354e707d86bee5262f60d69ba57d9
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2024-12-27 01:22:50 +0530

    Almost Done

--- game/src/API/Data.jsx
+++ game/src/API/Data.jsx
@@ -1,44 +1,43 @@
 import { useContext, useEffect, useState } from "react";
 import { ToogleContext } from "../Auth/Toogle";
-import Error1 from "../ErrorPage/Error1";
+
 import GamesList from "../Games/GameList";
 const Data=()=>{
     const [GamesGener ,setGamesGenere]=useState([])
     const {theme}=useContext(ToogleContext)
     const [acvtiveIndex,setactiveIndex]=useState()
-    const [loading ,setloading]=useState(true)
-    const [error ,seterror]=useState(null)
+  
 
-    const key="cd13302ff4c64e7bba696c7cfb5b5ae9"
+
+    const key="9b1bc84600374d0abdb13847e5ed31a0"
     // show the Data one Component render
-    useEffect(()=>{
-      const getGener=async()=>{
-        const link=`https://api.rawg.io/api/genres?key=${key}`
-        try{
-          const Result= await fetch(link)
-          if(!Result.ok){
-            throw new Error(`HTTP error ! ${Result.status}` )
-          }
-          const rep=await Result.json()
-          setGamesGenere(rep.results)
-        }catch(error){
-        seterror(error.message)
-       console.log("Fetching error".error);
-        }finally{
-          setloading(false);
+    const getGener = async () => {
+      const link = `https://api.rawg.io/api/genres?key=${key}`;
+      try {
+        const Result = await fetch(link);
+        if (!Result.ok) {
+          throw new Error(`HTTP error! status: ${Result.status}`);
         }
-      };
-      getGener()
-    },[])
+        const rep = await Result.json();
+        console.log(rep.results);
+        setGamesGenere(rep.results);
+      } catch (error) {
+        console.log("Fetching error", error);
+      }
+    };
+    
+    useEffect(() => {
+      getGener();
+    }, []);
+    
+      
     // for the Loading Section 
-    if (loading) { return <div>Loading...</div>; } 
-    if (error) { return <Error1 message={error} />; 
-  }
+    
 
     
     return(
   <section className={`mt-14  grid grid-cols-3 ${theme === "dark" ? "bg-black text-white" : null } `}>
-        <div className=" h-full  border-r-2 hidden md:block ">
+        <div className=" h-full   w-[70%]  border-r-2 hidden md:block  overflow-y-auto">
           <h2 className="dark:text-white   dark:hover:bg-slate-200  dark:hover:text-black font-special   hover:bg-gray-950 hover:text-white rounded-md px-2 py-1  cursor-pointer  m-2">Gener Section</h2>
           {GamesGener.map((items,index)=>{
            return (
@@ -49,10 +48,10 @@ const Data=()=>{
            )
           })}
         </div>
-        <div className= " col-span-3 md:col-span-2 h-full ">
+        <div className= "  col-span-3 md:col-span-2 h-full overflow-y ">
           <GamesList />
         </div>
-      </section>
+    </section>
 
     )
 }
