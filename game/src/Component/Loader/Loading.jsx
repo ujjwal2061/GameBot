77794d8785032d3f0f2b46e787d6e449f802d493
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2024-12-26 18:47:54 +0530

    Error section

--- game/src/Component/Loader/Loading.jsx
+++ game/src/Component/Loader/Loading.jsx
@@ -2,10 +2,10 @@ const Loading=()=>{
     return(
     <div className="flex flex-col justify-center items-center ">
       <img src='bike.gif' className="w-52 h-auto mb-5 flex-row  rounded-lg  justify-center" />
-      <div className="loader">Loading...</div>
+      <div className="loader">
+        <p className="font-jetbrains">Loding....</p>
+      </div>
     </div>
     )
 }
 export default Loading;
-
-``
