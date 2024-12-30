cc5ccab6c2ac7ad43803e5495bded083cce9b3a8
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2024-12-30 22:41:11 +0530

    Done With Singin Page

--- game/src/Component/Sigin.jsx
+++ game/src/Component/Sigin.jsx
@@ -1,4 +1,4 @@
-import  React, { useState } from 'react'
+import  { useState } from 'react'
 import {signInWithEmailAndPassword } from 'firebase/auth'; 
 import {useFirebaseAuth} from '../Auth/Fireauth' 
 import { useNavigate } from 'react-router';
